0f7bd2400206e56f8e75c2d3ddac3eefdefd4dbc
João Victor da Silva <jv782063@gmail.com>
2021-09-13 20:46:13 -0300

    configure some editor options

---
+++ lua/core/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+opt.hidden = true
+opt.number = true
+opt.autoindent = true
+opt.encoding = "UTF-8"
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+opt.scrolloff = 20
