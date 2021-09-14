7d4433c4fe6021a74de893a20a7a0766aab7918d
João Victor da Silva <jv782063@gmail.com>
2021-09-14 11:38:47 -0300

    map shortcuts to change buffer

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,2 +1,8 @@
+local map = vim.api.nvim_set_keymap
+
 -- nvimtree
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle <CR>", { noremap = true, silent = true })
+map("n", "<C-n>", ":NvimTreeToggle <CR>", { noremap = true, silent = true })
+
+-- bufferline
+map("n", "<TAB>", ":BufferLineCycleNext <CR>", { noremap = true, silent = true })
+map("n", "<S-TAB>", ":BufferLineCyclePrev <CR>", { noremap = true, silent = true })
