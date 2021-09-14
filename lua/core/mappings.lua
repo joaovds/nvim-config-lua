ea53ffe7b784bc8caf02104dcfb52737d1e5f9ef
João Victor da Silva <jv782063@gmail.com>
2021-09-14 15:40:52 -0300

    update coc.nvim with ctrl-space

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -13,3 +13,10 @@ map("n", "<C-f>", ":Telescope find_files <CR>", { noremap = true, silent = true 
 map("n", "<C-b>", ":Telescope buffers <CR>", { noremap = true, silent = true })
 map("n", "<C-g><Space>", ":Telescope git_commits <CR>", { noremap = true, silent = true })
 map("n", "<C-g><C-s>", ":Telescope git_status <CR>", { noremap = true, silent = true })
+
+-- coc.nvim
+if vim.fn.has('nvim') == 1 then
+  map("i", "<C-Space>", "coc#refresh()", { expr = true, silent = true })
+else
+  map("i", "<C-@>", "coc#refresh()", { expr = true, silent = true })
+end
